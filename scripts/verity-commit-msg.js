82e25ff5c6e770284e149c91377ffecbd06a45e1
kisoua <zhangyaphet@gmail.com>
2020-04-27 15:35:40 +0800

    feat: add eslint

--- scripts/verity-commit-msg.js
+++ scripts/verity-commit-msg.js
@@ -1,28 +1,24 @@
-const chalk = require("chalk");
-const msgPath = process.env.GIT_PARAMS;
-const msg = require("fs").readFileSync(msgPath, "utf-8").trim();
+import { bgRed, cyan, green, red } from 'chalk'
+const msgPath = process.env.GIT_PARAMS
+const msg = require('fs').readFileSync(msgPath, 'utf-8').trim()
 
-const commitReg = /^(revert: )?(feat|fix|docs|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release|dep)(\(.+\))?: .{1, 50}/;
+const commitReg = /^(revert: )?(feat|fix|docs|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release|dep)(\(.+\))?: .{1, 50}/
 
 if (!commitReg.test(msg)) {
-  console.log();
+  console.log()
   console.error(
-    `  ${chalk.bgRed.white(" ERROR ")} ${chalk.red(
-      `invalid commit message format.`
-    )}\n\n` +
-      chalk.red(
-        `  Proper commit message format is required for automated changelog generation. Examples:\n\n`
+    `  ${bgRed.white(' ERROR ')} ${red(`invalid commit message format.`)}\n\n` +
+      red(
+        `  Proper commit message format is required for automated changelog generation. Examples:\n\n`,
       ) +
-      `    ${chalk.green(`feat(compiler): add 'comments' option`)}\n` +
-      `    ${chalk.green(
-        `fix(v-model): handle events on blur (close #28)`
-      )}\n\n` +
-      chalk.red(`  See .github/COMMIT_CONVENTION.md for more details.\n`) +
-      chalk.red(
-        `  You can also use ${chalk.cyan(
-          `npm run commit`
-        )} to interactively generate a commit message.\n`
-      )
-  );
-  process.exit(1);
+      `    ${green(`feat(compiler): add 'comments' option`)}\n` +
+      `    ${green(`fix(v-model): handle events on blur (close #28)`)}\n\n` +
+      red(`  See .github/COMMIT_CONVENTION.md for more details.\n`) +
+      red(
+        `  You can also use ${cyan(
+          `npm run commit`,
+        )} to interactively generate a commit message.\n`,
+      ),
+  )
+  process.exit(1)
 }
